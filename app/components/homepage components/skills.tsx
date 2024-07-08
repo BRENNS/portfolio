8b189b10a1dba768fd5940ceaecdd34f237420d6
Steven Gang <steven@flat.fr>
2024-07-08 22:58:24 +0200

    feat(gitignore)

---
+++ app/components/homepage components/skills.tsx
@@ -0,0 +1,9 @@
+const Formation = () => {
+  return (
+    <section className={"h-screen w-full flex justify-center items-center"}>
+      formation
+    </section>
+  );
+};
+
+export default Formation;
